d91634bc26a3916812ed69559b256bbd128715d2
Ilya <i.kaltygin@icloud.com>
2022-12-05 18:37:11 +0700

    2. Доменная модель проекта "АвтоМаг" [#504897 #367319] внес изменения в схему auto_post

--- db/scripts/002_ddl_init_schema_auto_post.sql
+++ db/scripts/002_ddl_init_schema_auto_post.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS auto_post(
     id SERIAL PRIMARY KEY,
-    text VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
     created DATE NOT NULL ,
     auto_user_id INT NOT NULL REFERENCES auto_user(id)
 );
 
 comment on table auto_post is 'Таблица с объявлениями';
 comment on column auto_post.id is 'Идентификатор объявления';
-comment on column auto_post.text is 'Описание объявления';
+comment on column auto_post.description is 'Описание объявления';
 comment on column auto_post.created is 'Дата создания объявления';
 comment on column auto_post.auto_user_id is 'Идентификатор пользователя';
